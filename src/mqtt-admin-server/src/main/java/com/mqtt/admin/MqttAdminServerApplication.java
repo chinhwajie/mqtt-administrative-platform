72f1a82869b7e35c17182a3ea536491e419343a8
chj <jiejie02.chj@gmail.com>
2023-10-15 01:18:20 +0800

    Setup basic dev env

---
+++ src/mqtt-admin-server/src/main/java/com/mqtt/admin/MqttAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.mqtt.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttAdminServerApplication.class, args);
+    }
+
+}
